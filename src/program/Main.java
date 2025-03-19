2f81a516d765d53c0922fde3453bceb4639c7d7d
Gabriel Xavier Garcia <gabrielproaj@gmail.com>
2025-03-19 12:07:38 -0300

    Deixando o ambiente limpo.

--- src/program/Main.java
+++ src/program/Main.java
@@ -7,10 +7,11 @@ import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
 public class Main {
     public static void main(String[] args) {
+        
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
